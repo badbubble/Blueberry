26fbb4d534c3e8e76c7614e2229f6ccb9047fcf5
badbubble <liuhanjun369@gmail.com>
2024-07-23 08:48:59 +0800

    Initialize the logger and print an error message when errors occur.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"Blueberry/internal/router"
 	"Blueberry/pkg/k8s"
+	"Blueberry/pkg/logger"
 	"Blueberry/pkg/setting"
 	"fmt"
 )
 
 func main() {
+	// initialize settings
 	if err := setting.Init("config/dev.yaml"); err != nil {
+		fmt.Printf("Fail to load configuration: %v", err)
 		return
 	}
-	k8s.Init()
+	// create k8s client
+	if err := k8s.Init(); err != nil {
+		fmt.Printf("Fail to create k8s client: %v", err)
+		return
+	}
+	// initialize logger
+	if err := logger.Init(setting.Conf); err != nil {
+		fmt.Printf("Fail to initialize logger: %v", err)
+		return
+	}
 
 	r := router.SetupRouter()
 	err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port))
@@ -20,3 +32,5 @@ func main() {
 		return
 	}
 }
+
+
