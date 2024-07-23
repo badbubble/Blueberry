7eed15d619280afd131a49a94cf83150edf405af
badbubble <liuhanjun369@gmail.com>
2024-07-23 08:50:47 +0800

    add CodeGetPodDetailError

--- internal/controller/code.go
+++ internal/controller/code.go
@@ -8,14 +8,16 @@ const (
 	CodeParameterError
 	CodeInvalidParameter
 	CodeCreatePodError
+	CodeGetPodDetailError
 )
 
 var codeMsgMap = map[RespCode]string{
-	CodeSuccess:          "Success",
-	CodeServerBusy:       "The server is busy. Please try again later.",
-	CodeParameterError:   "The parameter you sent is not correct.",
-	CodeInvalidParameter: "Some key parameters have not been set.",
-	CodeCreatePodError:   "Fail to create a Pod",
+	CodeSuccess:           "Success",
+	CodeServerBusy:        "The server is busy. Please try again later.",
+	CodeParameterError:    "The parameter you sent is not correct.",
+	CodeInvalidParameter:  "Some key parameters have not been set.",
+	CodeCreatePodError:    "Fail to create a Pod",
+	CodeGetPodDetailError: "Fail to get the pod's information.",
 }
 
 func (r RespCode) Msg() string {
